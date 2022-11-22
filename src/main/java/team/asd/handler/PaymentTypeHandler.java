87a6a8c8025e4cc782b1636de9653bac128c7bac
Olena Syrotenko <syrotenko.career@gmail.com>
2022-11-22 11:07:47 +0200

    #44 Implemented type handlers for PropertyManagerInfo enums

---
+++ src/main/java/team/asd/handler/PaymentTypeHandler.java
@@ -0,0 +1,30 @@
+package team.asd.handler;
+
+import org.apache.ibatis.type.EnumTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import team.asd.constants.PaymentType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PaymentTypeHandler extends EnumTypeHandler<PaymentType> {
+	public PaymentTypeHandler(Class<PaymentType> type) {
+		super(type);
+	}
+
+	public void setNonNullParameter(PreparedStatement ps, int i, PaymentType parameter, JdbcType jdbcType) throws SQLException {
+		ps.setInt(i, parameter.getIntegerValue());
+	}
+
+	public PaymentType getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		Integer value = rs.getInt(columnName);
+		return rs.wasNull() ? null : PaymentType.getByInteger(value);
+	}
+
+	public PaymentType getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		Integer value = cs.getInt(columnIndex);
+		return cs.wasNull() ? null : PaymentType.getByInteger(value);
+	}
+}
