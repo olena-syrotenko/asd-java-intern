f8677dc6861ce94dd1f8aa58f0c205059793c5c7
Olena Syrotenko <syrotenko.career@gmail.com>
2022-12-06 10:45:13 +0200

    #57 Created endpoints documentation for ChannelPartnerController

--- src/main/java/team/asd/controller/ChannelPartnerController.java
+++ src/main/java/team/asd/controller/ChannelPartnerController.java
@@ -1,5 +1,9 @@
 package team.asd.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,51 +34,77 @@ public class ChannelPartnerController {
 		this.channelPartnerService = channelPartnerService;
 	}
 
+	@ApiOperation(value = "Get a channel partner by id", notes = "Returns a channel partner as per the id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed") })
 	@GetMapping("/channel-partner/{id}")
-	public ChannelPartnerDto getChannelPartnerById(@PathVariable Integer id) {
+	public ChannelPartnerDto getChannelPartnerById(@PathVariable @ApiParam(name = "id", value = "Channel partner id", example = "1") Integer id) {
 		return ChannelPartnerUtil.convertToDto(channelPartnerService.readById(id));
 	}
 
+	@ApiOperation(value = "Get a channel partner by abbreviation", notes = "Returns a channel partner as per the abbreviation")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed") })
 	@GetMapping("/channel-partners")
-	public List<ChannelPartnerDto> getChannelPartnersByAbbreviationMask(@RequestParam(name = "abbreviationMask") String abbreviationMask) {
+	public List<ChannelPartnerDto> getChannelPartnersByAbbreviationMask(
+			@RequestParam(name = "abbreviationMask") @ApiParam(name = "abbreviationMask", value = "Abbreviation of channel", example = "ABBR") String abbreviationMask) {
 		return channelPartnerService.readByAbbreviationMask(abbreviationMask)
 				.stream()
 				.map(ChannelPartnerUtil::convertToDto)
 				.collect(Collectors.toList());
 	}
 
+	@ApiOperation(value = "Get a channel partner by party id and state", notes = "Returns a channel partner as per the party id and state")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed") })
 	@GetMapping("/channel-partner")
-	public ChannelPartnerDto getChannelPartnerByPartyIdState(@RequestParam(name = "partyId", required = false) Integer partyId,
-			@RequestParam(name = "state", required = false) String state) {
+	public ChannelPartnerDto getChannelPartnerByPartyIdState(
+			@RequestParam(name = "partyId", required = false) @ApiParam(name = "partyId", value = "Party id", example = "1") Integer partyId,
+			@RequestParam(name = "state", required = false) @ApiParam(name = "state", value = "Channel partner state", example = "Created") String state) {
 		return ChannelPartnerUtil.convertToDto(channelPartnerService.readByPartyIdState(partyId, state));
 	}
 
+	@ApiOperation(value = "Get a manager to channel by PM id and channel partner id", notes = "Returns a manager to channel as per the PM id and channel partner id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed") })
 	@GetMapping("/manager-to-channel")
 	public ManagerToChannelDto readManagerByPropertyManagerIdChannelPartnerId(
-			@RequestParam(name = "propertyManagerId", required = false) Integer propertyManagerId,
-			@RequestParam(name = "channelPartnerId", required = false) Integer channelPartnerId) {
+			@RequestParam(name = "propertyManagerId", required = false) @ApiParam(name = "propertyManagerId", value = "Property manager id", example = "1") Integer propertyManagerId,
+			@RequestParam(name = "channelPartnerId", required = false) @ApiParam(name = "channelPartnerId", value = "Channel partner id", example = "2") Integer channelPartnerId) {
 
 		return ManagerToChannelUtil.convertToDto(channelPartnerService.readManagerByPropManagerIdChanPartnerId(propertyManagerId, channelPartnerId));
 	}
 
+	@ApiOperation(value = "Get a managers to channel by channel partner id and net rate settings", notes = "Returns a managers to channel as per the channel partner id and net rate")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully retrieved"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed") })
 	@GetMapping("/managers-to-channel")
-	public List<ManagerToChannelDto> readManagersByChannelPartnerIdNetRate(@RequestParam(name = "channelPartnerId") Integer channelPartnerId,
-			@RequestParam(name = "netRate", required = false) Integer netRate) {
+	public List<ManagerToChannelDto> readManagersByChannelPartnerIdNetRate(
+			@RequestParam(name = "channelPartnerId") @ApiParam(name = "channelPartnerId", value = "Channel partner id", example = "2") Integer channelPartnerId,
+			@RequestParam(name = "netRate", required = false) @ApiParam(name = "netRate", value = "Net rate settings", example = "0") Integer netRate) {
 		return channelPartnerService.readManagersByChannelPartnerIdNetRate(channelPartnerId, netRate)
 				.stream()
 				.map(ManagerToChannelUtil::convertToDto)
 				.collect(Collectors.toList());
 	}
 
+	@ApiOperation(value = "Create a new channel partner", notes = "Returns a channel partner with create options")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully created"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed and channel partner was not created") })
 	@PostMapping("/channel-partner/")
-	public ChannelPartnerDto createChannelPartner(@RequestBody @Valid ChannelPartnerDto channelPartnerDto) {
+	public ChannelPartnerDto createChannelPartner(
+			@RequestBody @Valid @ApiParam(name = "channelPartnerDto", value = "Channel partner object that needs to be saved") ChannelPartnerDto channelPartnerDto) {
 		ChannelPartner channelPartner = ChannelPartnerUtil.convertToEntity(channelPartnerDto);
 		channelPartnerService.create(channelPartner);
 		return ChannelPartnerUtil.convertToDto(channelPartner);
 	}
 
+	@ApiOperation(value = "Save a list of managers to channels", notes = "Returns a list of managers to channels with create options")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully created"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed and managers to channel were not created") })
 	@PostMapping("/managers-to-channels/")
-	public List<ManagerToChannelDto> createManagerToChannels(@RequestBody List<@Valid ManagerToChannelDto> managerToChannelDto) {
+	public List<ManagerToChannelDto> createManagerToChannels(
+			@RequestBody @ApiParam(name = "managerToChannelDto", value = "Managers to channels list that needs to be saved") List<@Valid ManagerToChannelDto> managerToChannelDto) {
 		List<ManagerToChannel> managerToChannels = managerToChannelDto.stream()
 				.map(ManagerToChannelUtil::convertToEntity)
 				.collect(Collectors.toList());
@@ -84,8 +114,12 @@ public class ChannelPartnerController {
 				.collect(Collectors.toList());
 	}
 
+	@ApiOperation(value = "Update a channel partner", notes = "Returns a channel partner with update options")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully created"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed and channel partner was not updated") })
 	@PutMapping("/channel-partner/")
-	public ChannelPartnerDto updateChannelPartner(@RequestBody @Valid ChannelPartnerDto channelPartnerDto) {
+	public ChannelPartnerDto updateChannelPartner(
+			@RequestBody @Valid @ApiParam(name = "channelPartnerDto", value = "Channel partner information that needs to be updated") ChannelPartnerDto channelPartnerDto) {
 		ChannelPartner channelPartner = ChannelPartnerUtil.convertToEntity(channelPartnerDto);
 		if (channelPartnerDto.getState() == null) {
 			channelPartner.setState(null);
@@ -100,6 +134,9 @@ public class ChannelPartnerController {
 		return ChannelPartnerUtil.convertToDto(channelPartner);
 	}
 
+	@ApiOperation(value = "Delete a channel partner", notes = "Returns an id of channel partner that was deleted")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Successfully deleted"),
+			@ApiResponse(code = 400, message = "Bad request - The request was malformed and channel partner was not deleted") })
 	@DeleteMapping("/channel-partner/{id}")
 	public Integer deleteChannelPartnerById(@PathVariable Integer id) {
 		channelPartnerService.delete(id);
--- src/main/java/team/asd/dto/ChannelPartnerDto.java
+++ src/main/java/team/asd/dto/ChannelPartnerDto.java
@@ -1,6 +1,7 @@
 package team.asd.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,34 +18,42 @@ import javax.validation.constraints.Size;
 public class ChannelPartnerDto {
 	@JsonProperty("id")
 	@Positive(message = "Id should be positive")
+	@ApiModelProperty(notes = "Channel partner id", example = "1")
 	private Integer id;
 
 	@JsonProperty("party_id")
 	@Positive(message = "Party id should be positive")
+	@ApiModelProperty(notes = "Party id", example = "1")
 	private Integer partyId;
 
 	@JsonProperty("abbreviation")
-	@Size(max = 6, message = "Abbreviation size cannot be more than 6 characters")
+	@Size(min = 3, max = 6, message = "Abbreviation size cannot be more than 6 characters")
+	@ApiModelProperty(notes = "A short identifier of channel partner", example = "ABBR")
 	private String abbreviation;
 
 	@JsonProperty("channel_name")
 	@Size(max = 45, message = "Channel name size cannot be more than 6 characters")
+	@ApiModelProperty(notes = "Channel name", example = "some channel")
 	private String channelName;
 
 	@JsonProperty("state")
 	@Size(max = 15, message = "State size cannot be more than 6 characters")
+	@ApiModelProperty(notes = "State of channel partner", example = "Created")
 	private String state;
 
 	@JsonProperty("commission")
 	@PositiveOrZero(message = "Commission should be positive or zero")
+	@ApiModelProperty(notes = "Commission of channel", example = "1.5")
 	private Double commission;
 
 	@JsonProperty("bp_commission")
 	@PositiveOrZero(message = "Bp commission should be positive or zero")
+	@ApiModelProperty(notes = "Commission for reservation changed by BookingPal", example = "3.0")
 	private Double bpCommission;
 
 	@JsonProperty("funds_holder")
 	@Min(value = 0, message = "Funds holder configuration should be 0 or 1")
 	@Max(value = 1, message = "Funds holder configuration should be 0 or 1")
+	@ApiModelProperty(notes = "Configuration for channel if it will charge funds for reservation", example = "0")
 	private Integer isFundsHolder;
 }
