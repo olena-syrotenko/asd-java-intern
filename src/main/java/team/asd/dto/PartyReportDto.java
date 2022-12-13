e5ee9de27211c6b24992ed9670e8186139a91845
Olena Syrotenko <syrotenko.career@gmail.com>
2022-12-13 13:40:42 +0200

    #63 Refactored PartyReportDto structure

--- src/main/java/team/asd/dto/PartyReportDto.java
+++ src/main/java/team/asd/dto/PartyReportDto.java
@@ -14,9 +14,41 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class PartyReportDto {
-	@JsonProperty("party")
-	@ApiModelProperty(notes = "General party information")
-	private PartyDto party;
+	@JsonProperty("id")
+	@ApiModelProperty(notes = "Party id", example = "1")
+	private Integer id;
+
+	@JsonProperty("name")
+	@ApiModelProperty(notes = "Name of party", example = "test party")
+	private String name;
+
+	@JsonProperty("state")
+	@ApiModelProperty(notes = "Party state", example = "Created")
+	private String state;
+
+	@JsonProperty("postal_address")
+	@ApiModelProperty(notes = "Postal address of party", example = "Mr John Smith. 132, My Street, Bigtown BG23 4YZ")
+	private String postalAddress;
+
+	@JsonProperty("email_address")
+	@ApiModelProperty(notes = "Email address of party", example = "john.smith@example.com")
+	private String emailAddress;
+
+	@JsonProperty("mobile_phone")
+	@ApiModelProperty(notes = "Mobile phone number of party", example = "(555) 555-1234")
+	private String mobilePhone;
+
+	@JsonProperty("password")
+	@ApiModelProperty(notes = "Hashed password", example = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824")
+	private String password;
+
+	@JsonProperty("currency")
+	@ApiModelProperty(notes = "Default currency for usage", example = "USD")
+	private String currency;
+
+	@JsonProperty("user_type")
+	@ApiModelProperty(notes = "User type", example = "Customer")
+	private String userType;
 
 	@JsonProperty("channel_partner")
 	@ApiModelProperty(notes = "Additional information for party with ChannelPartner type")
